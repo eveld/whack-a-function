e1ff9fa37b13ab2bcfa05e42e5939893226913ba
Nic Jackson <jackson.nic@gmail.com>
2019-07-07 10:00:56 +0100

    update

--- index.js
+++ index.js
@@ -10,51 +10,51 @@ const http = require('request');
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-    const agent = new WebhookClient({ request, response });
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-    function consul(agent) {
-        var percent = request.body.queryResult.parameters.canary;
+  function consul(agent) {
+    var percent = request.body.queryResult.parameters.canary;
 
-        if (percent > 100 || percent < 0) {
-            agent.add("Are you sure you know how percentages work?");
-            return;
-        }
-
-        var payload = {
-            "Kind": "service-splitter",
-            "Name": "api",
-            "Splits": [
-                {
-                    "Weight": 100 - percent,
-                    "Service": "api",
-                    "ServiceSubset": "v1"
-                },
-                {
-                    "Weight": percent,
-                    "Service": "api",
-                    "ServiceSubset": "v2"
-                }
-            ]
-        };
-
-        return new Promise(resolve => {
-            http({
-                url: 'http://consul.google.demo.gs/v1/config',
-                method: 'PUT',
-                json: payload
-            }, function (error, response, body) {
-                console.log(error);
-                if (!error) {
-                    resolve(body);
-                }
-            });
-        }).then(value => {
-            // process value here
-            agent.add('Ok done. So is voice ops the new git ops then? How are you planning to version control your voice? Storing MP3 in github?');
-        });
+    if (percent > 100 || percent < 0) {
+        agent.add("Are you sure you know how percentages work?");
+        return;
     }
+
+    var payload = {
+        "Kind": "service-splitter",
+        "Name": "api",
+        "Splits": [
+            {
+                "Weight": 100 - percent,
+                "Service": "api",
+                "ServiceSubset": "v1"
+            },
+            {
+                "Weight": percent,
+                "Service": "api",
+                "ServiceSubset": "v2"
+            }
+        ]
+    };
+
+    return new Promise(resolve => {
+        http({
+            url: 'http://consul.google.demo.gs/v1/config',
+            method: 'PUT',
+            json: payload
+        }, function (error, response, body) {
+            console.log(error);
+            if (!error) {
+                resolve(body);
+            }
+        });
+    }).then(value => {
+        // process value here
+        agent.add('Ok done. So is voice ops the new git ops then? How are you planning to version control your voice? Storing MP3 in github?');
+    });
+  }
 
   function whack(agent) {
     agent.ask(new MediaObject({
